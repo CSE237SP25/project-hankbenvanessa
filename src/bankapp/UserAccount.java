b8658a6ef63d9b1f21e5fffc2633f23a974b0486
benwa <benwa@Ben-Computin>
2025-04-09 10:08:58 -0500

    fin

--- src/bankapp/UserAccount.java
+++ src/bankapp/UserAccount.java
@@ -1,16 +1,21 @@
 package bankapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserAccount {
 	
 	private String username;
 	private String password;
 	private BankAccount myAccount;
+	private List<BankAccount> myAccounts = new ArrayList<>();
 
 
 	public UserAccount(String username, String password) {
 		this.username = username;
 		this.password = password;
 		this.myAccount = new BankAccount();
+		myAccounts.add(myAccount);
 	}
 	
 	public boolean checkPassword(String passwordAttempt) {
@@ -22,38 +27,72 @@ public class UserAccount {
 			return false;
 		}
 	}
+
 	
+	// Multiple account methods
+	public void createBankAccount() {
+		BankAccount newAccount = new BankAccount();
+		myAccounts.add(newAccount);
+	}
 	
-	public boolean accountDeposit(double amount) {
-		try {
-			myAccount.deposit(amount);
-		}
-		catch (IllegalArgumentException e) {
+	public int getMaxBankAccountID() {
+		return myAccounts.size();
+	}
+	
+	public boolean accountDeposit(double amount, int bankAccountNum) {
+		if (bankAccountNum < 1 || bankAccountNum > myAccounts.size()) {
 			return false;
 		}
-		return true;
+		else {
+			BankAccount account = myAccounts.get(bankAccountNum-1);
+			try {
+				account.deposit(amount);
+			}
+			catch (IllegalArgumentException e) {
+				return false;
+			}
+			return true;
+		}
+		
 	}
 	
 	
-	public boolean accountWithdrawal(double amount) {
-		try {
-			myAccount.withdraw(amount);
-		}
-		catch (IllegalArgumentException e) {
+	public boolean accountWithdrawal(double amount, int bankAccountNum) {
+		if (bankAccountNum < 1 || bankAccountNum > myAccounts.size()) {
 			return false;
 		}
-		return true;
+		else {
+			BankAccount account = myAccounts.get(bankAccountNum-1);
+			try {
+				account.withdraw(amount);
+			}
+			catch (IllegalArgumentException e) {
+				return false;
+			}
+			return true;
+		}
 	}
 	
 	
-	public double getAccountBalance() {
-		return this.myAccount.getCurrentBalance();
+	public double getAccountBalance(int bankAccountNum) {
+		if (bankAccountNum < 1 || bankAccountNum > myAccounts.size()) {
+			return 0;
+		}
+		else {
+			BankAccount account = myAccounts.get(bankAccountNum-1);
+			return account.getCurrentBalance();
+		}
 	}
 	
-	public void showTransactionHistory() {
-		this.myAccount.showTransactionHistory();
+	public void showTransactionHistory(int bankAccountNum) {
+		if (bankAccountNum < 1 || bankAccountNum > myAccounts.size()) {
+			return;
+		}
+		else {
+			BankAccount account = myAccounts.get(bankAccountNum-1);
+			account.showTransactionHistory();
+		}
 	}
-	
 	
 	
 }
